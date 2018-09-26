144b070610c218fe2f33fc2402f48b7c2c82ef43
rcosta <rui.fajozes@gmail.com>
2018-09-26 15:46:59 +0100

    dfd

--- myDockerImage/server/graphql/resolvers.js
+++ myDockerImage/server/graphql/resolvers.js
@@ -2,22 +2,21 @@ export default {
     Book: {
         author: (obj, args, context, info) => {
             
-            
-            
-             return context.dataloaders.authorById.load(obj.author_id);
+            //return context.Author.findOne({where: {id: obj.author_id}});
+            return context.dataloaders.authorById.load(obj.author_id);
         }
     },
     Author: {
         books: (obj, args, context, info) => {
-            //console.log(obj.id)
-            return context.dataloaders.booksByAuthorsIds.load(obj.id);
             
             //return context.Book.findAll({where: {author_id: obj.id}});
+            return context.dataloaders.booksByAuthorsIds.load(obj.id);
+            
         }
     },
   Query: { 
     authors: (obj, args, context, info) => {
-        return context.Author.findAll();
+        return context.Author.findAll( );
         
     },
     books: (obj, args, context, info) => {
--- myDockerImage/server/server.js
+++ myDockerImage/server/server.js
@@ -4,9 +4,9 @@ docker-compose down
 
 
 */
-
 import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
+import {ApolloServer} from 'apollo-server-express';
+
 import typeDefs from './graphql/schema'
 import resolvers from './graphql/resolvers'
 import fs from 'fs'
@@ -16,105 +16,123 @@ import http from 'http'
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
- 
+
 import Sequelize from 'sequelize';
 const sequelize = new Sequelize('practicedocker', 'postgres', 'password', {
-  host: 'db',
-  dialect:  'postgres' ,
-  operatorsAliases: false,
+    host: 'db',
+    dialect: 'postgres',
+    operatorsAliases: false,
 
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  }
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
- 
+
 var Author = require("./models/author.js")(sequelize, Sequelize);
 var Book = require("./models/book.js")(sequelize, Sequelize);
-     
+
 var DataLoader = require('dataloader');
- 
+
 const configurations = {
-  // Note: You may need sudo to run on port 443
-  production: { ssl: true, port: 443, hostname: '0.0.0.0' },
-  development: { ssl: false, port: 8080, hostname: '0.0.0.0' }
+    // Note: You may need sudo to run on port 443
+    production: {
+        ssl: true,
+        port: 443,
+        hostname: '0.0.0.0'
+    },
+    development: {
+        ssl: false,
+        port: 8080,
+        hostname: '0.0.0.0'
+    }
 }
 
 const environment = process.env.NODE_ENV || 'development'
 const config = configurations[environment]
 
- 
-const apollo = new ApolloServer({ typeDefs, resolvers ,
-    
-  context: () => {
-    return {
-        
-        dataloaders: {
-            booksByAuthorsIds: new DataLoader(function(authorIds)  {
-                
-                //console.log(authorIds)
-                
-                var promises =  authorIds.map(function(author_id){
-                    return Book.findAll({where: {author_id: author_id}});
-                    
-                })
-                
 
-                return Promise.all(promises);                
-                
-                
-                
-            }),
-            authorById: new DataLoader(function(author_id)  {
-                
-                
-                return Author.findAll(
-                        {
+const apollo = new ApolloServer({
+    typeDefs,
+    resolvers,
+
+    context: () => {
+        return {
+
+            dataloaders: {
+                booksByAuthorsIds: new DataLoader(function(authorIds) {
+
+                    //console.log(authorIds)
+
+                    var promises = authorIds.map(function(author_id) {
+                        return Book.findAll({
                             where: {
-                                id: author_id 
+                                author_id: author_id
                             }
                         });
-            })
-        },        
-       
-        
-      Author: Author,
-      Book: Book,
-    };
-  },
+
+                    })
+
+
+                    return Promise.all(promises);
+
+
+
+                }),
+                authorById: new DataLoader(function(author_id) {
+
+
+                    return Author.findAll({
+                        where: {
+                            id: author_id
+                        }
+                    });
+                }),
+                /*authorsAll: new DataLoader(function(xxx) {
+                    return Author.findAll( );
+                })*/
+            },
+
+
+            Author: Author,
+            Book: Book,
+        };
+    },
 
 });
 
 
 const app = express()
-apollo.applyMiddleware({ app })
+apollo.applyMiddleware({
+    app
+});
 
 // Create the HTTPS or HTTP server, per configuration
 var server;
 
 if (config.ssl) {
-  // Assumes certificates are in .ssl folder from package root. Make sure the files
-  // are secured.
-  server = https.createServer(
-    {
-      key: fs.readFileSync(`./ssl/${environment}/server.key`),
-      cert: fs.readFileSync(`./ssl/${environment}/server.crt`)
-    },
-    app
-  )
+    // Assumes certificates are in .ssl folder from package root. Make sure the files
+    // are secured.
+    server = https.createServer({
+            key: fs.readFileSync(`./ssl/${environment}/server.key`),
+            cert: fs.readFileSync(`./ssl/${environment}/server.crt`)
+        },
+        app
+    )
 } else {
-  server = http.createServer(app)
+    server = http.createServer(app)
 }
 
 // Add subscription support
 apollo.installSubscriptionHandlers(server)
 
-server.listen({ port: config.port }, () =>
-  console.log(
-    '🚀 Server ready at',
-    `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${apollo.graphqlPath}`
-  )
+server.listen({
+        port: config.port
+    }, () =>
+    console.log(
+        '🚀 Server ready at',
+        `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${apollo.graphqlPath}`
+    )
 )
-
