07ca5789199fc2c14339db9c875f81a0809a1c67
Rui Costa <rcosta@grupopie.com>
2019-07-23 18:29:25 +0100

    sdfs

--- estalido/ge/generate.js
+++ estalido/ge/generate.js
@@ -7,7 +7,7 @@
 //nor18 = o0=nor22?nor14:1    o1=nor23?o0:1    
 //nor18 = o0=nor23?nor14:1    o1=nor22?o0:1 
 
-var relayCount = 1;
+var relayCount = 3;
 
 function print(relays) {
     var text = "";
@@ -32,111 +32,78 @@ function printX(pin) {
 
 
 var xxx = [
+    "a0",
+    "b0",
     "cn",
     "m",
-
-
-    // "a0", 
-    // "b0",
-    // "!s2",
-    // "!s3",
-    // "nor22",
-    // "nor23",
-    // "nor14",
+    "s0",
+    "s1",
+    "s2",
+    "s3",
+    "xor1"
      
 
   ];
 
-
-//nor4 : o0=b0?!s0:!s1    o1=a0?0:o0
-//nor5 : o0=b0?!s3:!s2    o1=a0?o0:1
+ 
 
 
 var real = [];
-for(var i = 0; i< Math.pow(2,2); i++) {
 
-    /* 
-    var a0 = (i & 1)>0;
-    var b0 = (i & 2)>1;
-    var s2 = (i & 4)>2;
-    var s3 = (i & 8)>3;
-    var not12 = !b0;
-    var and9 = not12 & s2 & a0;
-   var and10 = a0 & s3 & b0;
-    var nor5 = !(and9 | and10);
-    */
+
+
+
+for(var i = 0; i< Math.pow(2,8); i++) {
+
+    var a0 = ((i & 1)>0)?1:0;
+    var b0 = ((i & 2)>1)?1:0;
+    var cn =( (i & 4)>2)?1:0;
+    var m = ((i & 8)>3)?1:0;
+    var s0 = ((i & 16)>4)?1:0;
+    var s1 = ((i & 32)>5)?1:0;
+    var s2 = ((i & 64)>6)?1:0;
+    var s3 =( (i & 128)>7)?1:0;
+
+    var result = a0 ? (b0?s0:s1) : (b0?s2:s3) ;
+
+    if(!m){
+        result = result+ a0 + cn;
+    } 
+
 
  
-    var cn = (i & 1)>0;
-    var m = (i & 2)>1;
-    var not11 = !m;
-    var nand2 = !(cn & not11);
+    // console.log(
+    //     a0?0:1,
+    //     b0?0:1,
+    //     cn?0:1,
+    //     m?0:1,
+    //     s0?0:1,
+    //     s1?0:1,
+    //     s2?0:1,
+    //     s3?0:1
+    // );
  
-/*
-   var cn = (i & 1)>0;
-   var m = (i & 2)>1;
-   var nor4 = (i & 4)>2;
-   var nor5 = (i & 8)>3;
-
-
-   var nor22 = (i & 16)>4;
-   var nor23 = (i & 32)>5;
-
-    var not11 = !m;
-
-    var nand2 = !(cn & not11);
-
-    var and16 = not11 & nor5 & cn;
-    var and15 = not11 & nor4;
-    var nor14 = !(and15 & and16);
-
-    
-
-    var and21 = not11 & nor23 & nor5 & cn;
-    var and20 = not11 & nor4 & nor23;
-    var and19 = not11 & nor22;
-    var nor18 = !(and19 & and20 & and21);
-*/
-    // var nor5 = (i & 8)>3;
-    // var nor22 = (i & 16)>4;
-    // var nor23 = (i & 32)>5;
-    
-    
- 
-
-    // var and6 = a0;
-    
-    
-    // var and7 = b0 & s0;
-    // var and8 = s1 & not12;
-    // var nor4 = !(and6 | and7 | and8);
-    //var xor3 = nor4 ^nor5;
-    
-    //var xor1 = nand2 ^ xor3 ;
-
-    //  var and16 = not11 & nor5 & cn;
-    //  var and15 = not11 & nor4;
-    //  var nor14 = !(and15 & and16);
-
-    // var and21 = not11 & nor23 & nor5 & cn;
-    // var and20 = not11 & nor4 & nor23;
-    // var and19 = not11 & nor22;
-    // var nor18 = !(and19 & and20 & and21);
-
     real.push( {
         i: [
-           ! cn,
+            a0,
+            b0,
+            cn,
             m,
+            //s0,
+            //s1,
+            s2,
+            s3,
         ],
         o: [
-            nand2
+            
+            ((result & 2)>1)?0:1
         ]
     });
-
-    inCount =2
-    outCount = 1;
 }
   
+ 
+inCount =6
+outCount = 1;
  
 
 
