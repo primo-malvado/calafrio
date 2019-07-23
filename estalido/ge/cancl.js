fd8ff14f0c5a3c4f383bee0894e11f3aab4aea8d
Rui Costa <rui.fajozes@gmail.com>
2019-07-23 22:18:57 +0100

    sdf

--- estalido/ge/cancl.js
+++ estalido/ge/cancl.js
@@ -21,11 +21,39 @@ for(var i = 0; i< Math.pow(2,8); i++) {
         result = result+ a0 + cn;
     } 
  
-    // console.log(m, result,
-    //     ((result & 2)>1)?1:0,
-    //     ((result & 1)>0)?1:0,);
 
+    var generate = 0;
+    if(a0==1 &&   m == 0){
+        if(b0 == 0 && s1 == 1){
+            generate = 1;
+        }
+        if(b0 == 1 &&  s0 == 1){
+            generate = 1;
+        }
+    }
+
+    var propagate = 0;
+    if( m == 0){
+        if(a0 == 0 && b0 == 0 && s3 == 1){
+            propagate = 1;
+        }
+         if(a0 == 0 && b0 == 1 && s2 == 1){
+            propagate = 1;
+        }
+         if(a0 == 1 && b0 == 0 && s1 == 0){
+              propagate = 1;
+        }
+         if(a0 == 1 && b0 == 1 && s0 == 0){
+              propagate = 1;
+        }
  
+    }
+
+    var c = generate | ( propagate & cn); 
+
+ if( ((result & 2)>1)  != c){
+
+
     console.log(
         a0?1:0,
         b0?1:0,
@@ -37,8 +65,10 @@ for(var i = 0; i< Math.pow(2,8); i++) {
         s3?1:0,
           ((result & 2)>1)?1:0,
         ((result & 1)>0)?1:0,
+         c,
     );
- 
+  }
+
 }
   
  
