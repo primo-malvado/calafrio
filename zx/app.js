9a9ba2c95c696299fdecdcbad66ee03fc473df83
Rui Costa <rcosta@grupopie.com>
2019-09-12 18:26:43 +0100

    sdf

--- zx/app.js
+++ zx/app.js
@@ -1,3 +1,30 @@
+colors = [
+    [
+        [0,0,0],
+        [0,0,192],
+        [192,0,0],
+        [192,0,192],
+        [0,192,0],
+        [0,192,192],
+        [192,192,0],
+        [192,192,192],
+
+    ],
+
+    [
+        [0,0,0],
+        [0,0,255],
+        [255,0,0],
+        [255,0,255],
+        [0,255,0],
+        [0,255,255],
+        [255,255,0],
+        [255,255,255],
+
+    ],
+
+]
+
 function FloydSteinbergDithering( errorMultiplier, data, w, h )
 {
 
@@ -9,185 +36,76 @@ function FloydSteinbergDithering( errorMultiplier, data, w, h )
 
     var x, y, xx, yy, r, g, b;
 
+   //red
+   var error = [];
+
+   for( y = 0; y < h; y++)
+   {
+       error.push( new Float32Array( w ) );
+   }
+
+   for( y = 0; y < h; y++ )
+   {
+
+       for (x = 0; x < w; x++)
+       {
+           var id = ( ( y * w ) + x ) * 4;
 
 
-    //red
-    var error = [];
+           var avg = (data[ id ]+ data[ id + 1 ] + data[ id + 2 ])/3 ;
+           avg -= error[y][x] * errorMultiplier;
 
-    for( y = 0; y < h; y++)
-    {
-        error.push( new Float32Array( w ) );
-    }
+           var e = 0;
+           if (avg < 128 )
+           {
+               e = -avg;
+               avg = 0;
+           }
+           else
+           {
+               e = 255 - avg;
+               avg = 255;
+           }
 
-    for( y = 0; y < h; y++ )
-    {
+           data[ id ] = avg;
+           data[ id + 1 ] = avg;
+           data[ id + 2 ] = avg;
 
-        for (x = 0; x < w; x++)
-        {
-            var id = ( ( y * w ) + x ) * 4;
+           data[ id + 3 ] = 255;
 
+           for( yy = 0; yy < 3; yy++)
+           {
+               for ( xx = -2; xx <= 2; xx++)
+               {
+                   if( y + yy < 0 || h <= y + yy
+                   ||  x + xx < 0 || w <= x + xx) continue;
 
-            var avg = data[ id ];
-            avg -= error[y][x] * errorMultiplier;
-
-            var e = 0;
-            if (avg < 128 )
-            {
-                e = -avg;
-                avg = 0;
-            }
-            else
-            {
-                e = 255 - avg;
-                avg = 255;
-            }
-
-            data[ id ] = avg;
-            //data[ id + 1 ] = avg;
-            //data[ id + 2 ] = avg;
-
-            data[ id + 3 ] = 255;
-
-            for( yy = 0; yy < 3; yy++)
-            {
-                for ( xx = -2; xx <= 2; xx++)
-                {
-                    if( y + yy < 0 || h <= y + yy
-                    ||  x + xx < 0 || w <= x + xx) continue;
-
-                    error[ y + yy ][ x + xx ] += e * filter[ yy ][ xx + 2 ];
-                }
-            }
-        }
-    }
-    //green
-    
-    var error = [];
-
-    for( y = 0; y < h; y++)
-    {
-        error.push( new Float32Array( w ) );
-    }
-
-    for( y = 0; y < h; y++ )
-    {
-
-        for (x = 0; x < w; x++)
-        {
-            var id = ( ( y * w ) + x ) * 4;
-
- 
-            var avg = data[ id + 1 ];
-
-            avg -= error[y][x] * errorMultiplier;
-
-            var e = 0;
-            if (avg < 128 )
-            {
-                e = -avg;
-                avg = 0;
-            }
-            else
-            {
-                e = 255 - avg;
-                avg = 255;
-            }
-
-            //data[ id ] = avg;
-            data[ id + 1 ] = avg;
-            //data[ id + 2 ] = avg;
-
-            data[ id + 3 ] = 255;
-
-            for( yy = 0; yy < 3; yy++)
-            {
-                for ( xx = -2; xx <= 2; xx++)
-                {
-                    if( y + yy < 0 || h <= y + yy
-                    ||  x + xx < 0 || w <= x + xx) continue;
-
-                    error[ y + yy ][ x + xx ] += e * filter[ yy ][ xx + 2 ];
-                }
-            }
-        }
-    }
-
-
-
-
-   //blue
-    
-    var error = [];
-
-    for( y = 0; y < h; y++)
-    {
-        error.push( new Float32Array( w ) );
-    }
-
-    for( y = 0; y < h; y++ )
-    {
-
-        for (x = 0; x < w; x++)
-        {
-            var id = ( ( y * w ) + x ) * 4;
-
- 
-            var avg = data[ id + 2 ];
-
-            avg -= error[y][x] * errorMultiplier;
-
-            var e = 0;
-            if (avg < 128 )
-            {
-                e = -avg;
-                avg = 0;
-            }
-            else
-            {
-                e = 255 - avg;
-                avg = 255;
-            }
-
-            data[ id + 2 ] = avg;
-
-            data[ id + 3 ] = 255;
-
-            for( yy = 0; yy < 3; yy++)
-            {
-                for ( xx = -2; xx <= 2; xx++)
-                {
-                    if( y + yy < 0 || h <= y + yy
-                    ||  x + xx < 0 || w <= x + xx) continue;
-
-                    error[ y + yy ][ x + xx ] += e * filter[ yy ][ xx + 2 ];
-                }
-            }
-        }
-    }
-
-
+                   error[ y + yy ][ x + xx ] += e * filter[ yy ][ xx + 2 ];
+               }
+           }
+       }
+   }
 
 
     return data;
 }
 
 
-  function loadCanvas(dataURL) {
+function loadCanvas(dataURL) {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
 
-    // load image from data url
     var imageObj = new Image();
+    
     imageObj.onload = function() {
-      context.drawImage(this, 0, 0);
+        context.drawImage(this, 0, 0);
 
 
-      var w = canvas.width ;
-      var h = canvas.height;
+        var w = canvas.width ;
+        var h = canvas.height;
 
-      console.time( 'dither' );
-
-      /*
+ 
+ /*
       var cW = w/8;
       var cH = h/8;
       
@@ -197,27 +115,27 @@ function FloydSteinbergDithering( errorMultiplier, data, w, h )
                 
 
                 var imgData = context.getImageData( iw*8,ih*8,8,8 );
+
                 imgData.data = FloydSteinbergDithering(.75, imgData.data, 8, 8 );
                 context.putImageData( imgData, iw*8,ih*8 );
             }
         }
 
-        */
+
+*/
+
 
         var imgData = context.getImageData( 0,0, w,h );
+        var original = imgData.data.map(function(a) {return a;} )
+
+        
         imgData.data = FloydSteinbergDithering(.75, imgData.data, w,h);
         context.putImageData( imgData, 0,0 );
-
-
-
-
-
-        console.timeEnd( 'dither' );
+ 
     };
 
     imageObj.src = dataURL;
   }
 
 
- loadCanvas("watchmen.png");
-//loadCanvas("eva.png");
+ loadCanvas("watchmen.png"); 
